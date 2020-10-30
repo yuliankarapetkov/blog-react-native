2699392f13a0630cfb301d2e8dec01f695178f31
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2020-10-30 12:40:58 +0200

    feat: pass posts from BlogContext

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,9 +3,17 @@ import React from 'react';
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
+    const posts = [
+        { title: 'Post #1' },
+        { title: 'Post #2' },
+        { title: 'Post #3' },
+    ];
+
     return (
-        <BlogContext.Provider>
+        <BlogContext.Provider value={posts}>
             {children}
         </BlogContext.Provider>
     )
 };
+
+export default BlogContext;
