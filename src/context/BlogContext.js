24265668b70c7cc69c30074883b9bbbea0c870bd
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2020-10-30 13:06:34 +0200

    feat: use reducer in BlogContext

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,20 +1,29 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
 const BlogContext = React.createContext();
 
-export const BlogProvider = ({ children }) => {
-    const initialSstate = [
-        { title: 'Post #1' },
-        { title: 'Post #2' },
-        { title: 'Post #3' },
-    ];
+const initialState = [
+    { title: 'Post #1' },
+    { title: 'Post #2' },
+    { title: 'Post #3' },
+];
 
-    const [posts, setPosts] = useState(initialSstate);
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_POST':
+            return [...state, { title: `Post #${state.length + 1}` }];
+        default:
+            return state;
+    }
+};
+
+export const BlogProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
     
-    const addPost = () => setPosts([...posts, { title: `Post #${posts.length + 1}` }]);
+    const addPost = () => dispatch({ type: 'ADD_POST' })
 
     return (
-        <BlogContext.Provider value={{ posts, addPost }}>
+        <BlogContext.Provider value={{ posts: state, addPost }}>
             {children}
         </BlogContext.Provider>
     )
