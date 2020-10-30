8735b24c27afb77bf3b2f282e96795718bb2b3c4
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2020-10-30 12:07:45 +0200

    feat: wrap App inside BlogProvider

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 import IndexScreen from './src/screens/IndexScreen';
+import { BlogProvider } from './src/context/BlogContext';
 
 const navigator = createStackNavigator({
   Index: IndexScreen,
@@ -12,4 +13,12 @@ const navigator = createStackNavigator({
   }
 });
 
-export default createAppContainer(navigator);
+const App = createAppContainer(navigator);
+
+export default () => {
+  return (
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+  );
+};
