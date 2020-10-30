ac628a561758945aa1786b1bce515c2d6050c60a
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2020-10-30 15:08:27 +0200

    fix: styling improvements

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 import { Context } from '../context/BlogContext';
 
@@ -17,13 +18,33 @@ const IndexScreen = () => {
                 data={state}
                 keyExtractor={({ title }) => title}
                 renderItem={({ item }) => {
-                    return <Text>{item.title}</Text>
+                    return (
+                        <View style={styles.post}>
+                            <Text style={styles.title}>{item.title}</Text>
+                            <Feather style={styles.icon} name="trash" />
+                        </View>
+                    );
                 }}
             />
         </View>
     )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    post: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        borderTopWidth: 1,
+        borderColor: 'gray'
+    },
+    title: {
+        fontSize: 18
+    },
+    icon: {
+        fontSize: 24
+    }
+});
 
 export default IndexScreen;
